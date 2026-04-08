5c128a5a5b8c2ec71c418773399298423488ba05
User <user@example.com>
2026-04-08 22:08:59 +0500

    fix(config): update open-next configuration as per cloudflare adapter specs

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,24 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
-
-const config: OpenNextConfig = {
-    default: {
-        runtime: "edge",
+export default {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "direct",
     },
+  },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "direct",
+    },
+  },
 };
-
-export default config;
